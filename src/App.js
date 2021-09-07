0b25ac654eb3a5b3425203ca1c6755671f73b78a
insider.og <marionrosete1@gmail.com>
2021-09-07 09:08:38 +0800

    Redirect if token is present

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import React from 'react'
 import Login from './component/Login'
 import Register from './component/Register'
 import Dashboard from './component/auth/Dashboard'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom';
 import ProtectedRoutes from './ProtectedRoutes';
 
 
 
 function App(){
-    
+    const token=localStorage.getItem('token')
   return (
     <>
       
       <Router>
         <Switch>
             <Route exact path="/Register" component={Register}/>
-            <Route exact path="/" component={Login}/>
+            <Route exact path="/" component={Login}>
+              {token !=null ? <Redirect to="/dashboard" /> : <Login />}
+            </Route>
         </Switch>
-          <ProtectedRoutes exact path="/Dashboard" component={Dashboard} props={localStorage.getItem('status')} /> 
+          <ProtectedRoutes exact path="/Dashboard" component={Dashboard} /> 
+          
       </Router>
      
     </>
