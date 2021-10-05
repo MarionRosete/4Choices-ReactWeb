2a66124bfe34872d8b3427163129f0af31f7349b
insider.og <marionrosete1@gmail.com>
2021-10-05 14:46:46 +0800

    Design Needed!

--- src/component/AttendeesExam.js
+++ src/component/AttendeesExam.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import {useState} from 'react'
 
 function AttendeesExam(props){
-    console.log(props.location.state)
+   
 
     const success = props.location.state.status
     const name = props.location.state.name
     const instructor = props.location.state.instructor
     const data =props.location.state.data
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+   
+	function handleAnswerOptionClick(answer,value){
+		if (value===answer) {
+			setScore(score + 1);
+            console.log("Correct!")
+		}
+       
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < data.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
     //Random Exam
       for(let i=data.length-1;i>0;i--){
             const j = Math.floor(Math.random()*(i+1));
@@ -16,22 +34,27 @@ function AttendeesExam(props){
         }
     
         
-     
-    
-   console.log(data)
+      
     return (
     <>
-    
+ 
+
     {success === true?
         <>
+        {showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {data.length}
+				</div>
+			) : (<>
          Exam Name: {name}<br/>
-         Instructor: {instructor}
-            {data.map((item)=><div key={item.id}> {item.id}. {item.question} <br/> 
-           {item.answer1}<br/>
-           {item.answer2}<br/>
-           {item.answer3}<br/>
-           {item.answer4}</div>)}
-
+         Instructor: {instructor} <br/> 
+          <span>Question:{currentQuestion + 1}</span><br/>
+          {data[currentQuestion].question} <br/> 
+            <button value={1} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer1}</button><br/>
+            <button value={2} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer2}</button><br/>
+            <button value={3} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer3}</button><br/>
+            <button value={4} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer4}</button>
+           </> )}
         </>
         :
         <>
