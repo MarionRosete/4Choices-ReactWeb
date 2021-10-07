e32737cab3dfd01b5c1cc3e65c42db197038ad5e
Paolo-Paredes <20180014160@my.xu.edu.ph>
2021-10-07 14:01:04 +0800

    Minor Changes createQnA, Needs Revision.
    code from Iris

--- src/component/auth/CreateQandA.js
+++ src/component/auth/CreateQandA.js
@@ -10,7 +10,7 @@ function CreateQandA(props) {
     const [answer, setAnswer] = useState([]);
     const [msg, setMsg] = useState([]);
     const handleQuestions = (e)=>{
-       
+
         const qa = {question, answer1, answer2, answer3, answer4,answer};
         fetch(`http://localhost:8000/api/dashboard/createqa/${exam.code}`,{
             headers:{"Content-Type":'application/json', "accept":'application/json',  Authorization: `Bearer ${localStorage.getItem('token')}`},
@@ -24,50 +24,42 @@ function CreateQandA(props) {
         setAnswer4("");
         setAnswer("")
         for (const checkbox of document.querySelectorAll('.myCheckBox')) {
-            
-            checkbox.checked = true 
-            checkbox.checked = false 
+
+            checkbox.checked = true
+            checkbox.checked = false
             }
     }
     return (
-        <div className="md:px-40">
-                <div className="bg-blue-100 rounded-2xl p-5 md:py-3 md:px-8">
-                    
-                    <div className="flex flex-col bg-blue-100 rounded-2xl space-y-8">
-                        <div className="flex items-center text-blue-800 font-medium">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                        </svg>
-                        {exam.subject}
-                        </div>
-                        {msg}
-                    
-                        <textarea className="resize-none border rounded-md" rows="4"  type="text" required value={question} placeholder="Write your question here" onChange={(e)=>setQuestion(e.target.value)}/>
-                      
-                        <div>
-                            <div className="flex justify-center items-center space-x-3">
-                                <input className="myCheckBox" type="checkbox" value={1} onChange={(e)=>setAnswer(e.target.value)}/><input className="bg-blue-900 border rounded-md p-2 my-2 w-3/5" type="text" placeholder="Answer option 1" required value={answer1}  onChange={(e)=>setAnswer1(e.target.value)}/>
-                            </div>
-                            <div className="flex justify-center items-center space-x-3">
-                                <input className="myCheckBox" type="checkbox" value={2} onChange={(e)=>setAnswer(e.target.value)}/><input className="bg-blue-900 border rounded-md p-2 my-2 w-3/5" type="text" placeholder="Answer option 2" required value={answer2}  onChange={(e)=>setAnswer2(e.target.value)}/>
-                            </div>
-                            <div className="flex justify-center items-center space-x-3">
-                                <input className="myCheckBox" type="checkbox" value={3} onChange={(e)=>setAnswer(e.target.value)}/><input className="bg-blue-900 border rounded-md p-2 my-2 w-3/5" type="text" placeholder="Answer option 3" required value={answer3}  onChange={(e)=>setAnswer3(e.target.value)}/>
-                            </div>
-                            <div className="flex justify-center items-center space-x-3">
-                                <input className="myCheckBox" type="checkbox" value={4} onChange={(e)=>setAnswer(e.target.value)}/><input className="bg-blue-900 border rounded-md p-2 my-2 w-3/5" type="text" placeholder="Answer option 4" required value={answer4}  onChange={(e)=>setAnswer4(e.target.value)}/>
-                            </div>
-                        </div>
-                        <div className = "flex justify-end p-3">
-                            <button className= "text-blue-900 hover:text-blue-600 font-medium text-lg px-4 py-1.5 items-end" onClick={()=> window.location.replace("/Dashboard")}>Cancel </button>
-                            <button className= "block bg-blue-900 hover:bg-blue-600 shadow-lg text-white py-1.5 px-4 rounded-md " onClick={handleQuestions} >Submit </button>
-                        </div>
-                      
-               
-                    </div>
-                </div>
-        </div>
+      <>
+        <div class="min-h-screen p-20 bg-white">
+            <div class="flex flex-col justify-center bg-blue-100 rounded-2xl p-4 md:p-16 h-full">
+              <span class=" text-blue-900 font-medium text-xl py-5 px-10">Earth Science</span>
+              <span class=" text-blue-900 font-medium text-sm px-10 py-4">Question 1</span>
+              <input class="h-5 p-6 border rounded-md" type="text" placeholder="Write Question here" />
+
+                      <div className="flex grid grid-cols-2  place-items-center gap-y-3 gap-x-3 p-4">
+                            <input type="checkbox" class="w-8 h-5 checked:bg-white checked:border "/>
+                            <input className="bg-blue-900 text-white h-5 p-6 border rounded-lg" type="text" placeholder="Write Answer" />
+                            <input type="checkbox" class="w-8 h-5 checked:bg-white checked:border "/>
+                            <input className="bg-blue-900 text-white h-5 p-6 border rounded-lg" type="text" placeholder="Write Answer" />
+                            <input type="checkbox" class="w-8 h-5 checked:bg-white checked:border "/>
+                            <input className="bg-blue-900 text-white h-5 p-6 border rounded-lg" type="text" placeholder="Write Answer" />
+                            <input type="checkbox" class="w-8 h-5 checked:bg-white checked:border "/>
+                            <input className="bg-blue-900 text-white h-5 p-6 border rounded-lg" type="text" placeholder="Write Answer" />
+                      </div>
+
+                      <div class="px-10 py-4">
+                            <button class="block bg-blue-900 text-white hover:text-blue-500 font-small text-sm px-4 py-1.5 items-end rounded-md px-12">Add question </button>
+                      </div>
+
+                      <div class = "flex justify-end p-10">
+                          <button class= "text-blue-900 hover:text-blue-600 font-medium text-lg px-4 py-1.5 items-end"> Cancel </button>
+                          <button class= "block bg-blue-900 hover:bg-blue-600 shadow-lg text-white py-1.5 px-4 rounded-md ">Next </button>
+                      </div>
+
+            </div>
+          </div>
+          </>
     )
 }
 
