5e31985fcef2436d1fe392d067715b52055fb251
insider.og <marionrosete1@gmail.com>
2021-10-12 09:53:28 +0800

    :P

--- src/component/auth/QandA.js
+++ src/component/auth/QandA.js
@@ -43,14 +43,18 @@ function QandA(props){
 				</div>
 			) : (<>
           <div className="md:px-40 md:p-20 p-8">
-                <div className="text-blue-900  bg-blue-200 "><p className='text-lg font-bold space-y-x'>Items:{currentQuestion}/{data.length}</p><div className="justify-center items-center p-24">  {data[currentQuestion].question}</div> </div><br/>
+            <div className="text-blue-900  bg-blue-200 "><p className='text-lg font-bold space-y-x'>Items:{currentQuestion}/{data.length}</p>
+            <div className="justify-center items-center p-24">  {data[currentQuestion].question}</div> 
+            <div className="text-blue-900  bg-blue-200 "><p className='text-lg font-bold space-y-x'>Correct answer:{data[currentQuestion].answer}</p></div>
+            </div>
+            <br/>
             <div className="flex justify-between items-center md:space-x-32">
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={1} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >{data[currentQuestion].answer1}</button><br/>
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={2} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >{data[currentQuestion].answer2}</button>
+                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={1} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >1.{data[currentQuestion].answer1}</button><br/>
+                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={2} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >2.{data[currentQuestion].answer2}</button>
             </div>
             <div className="flex justify-between items-center md:space-x-32">
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={3} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >{data[currentQuestion].answer3}</button><br/>
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={4} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >{data[currentQuestion].answer4}</button>
+                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={3} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >3.{data[currentQuestion].answer3}</button><br/>
+                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={4} onClick={() => handleAnswerOptionClick(data[currentQuestion])} >4.{data[currentQuestion].answer4}</button>
             </div>
             </div>
         </>)}
