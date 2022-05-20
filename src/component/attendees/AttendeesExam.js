027be6fda619eb2177c9d21fdff7dcea51c4bf0c
MarionRosete <marion.rosete@fligno.com>
2022-05-20 16:08:34 +0800

    spaghetti

--- src/component/attendees/AttendeesExam.js
+++ src/component/attendees/AttendeesExam.js
@@ -31,7 +31,9 @@ function AttendeesExam(props){
 
     {data.length === 0?
         <>
-       <h1>No Question And Answers Exists</h1>
+       <h1>
+           No Question And Answers Exists
+        </h1>
         </>
         :
         <>
@@ -39,19 +41,53 @@ function AttendeesExam(props){
             {showScore ? (
 				<div className='text-blue-900 flex bg-blue-200 justify-center items-center p-24'>
 					You scored {score} out of {data.length}
-                    
 				</div>
-			) : (<>
+			) : (
+            <>
             <div className="md:px-40 md:p-20 p-8">
-                <div className="text-blue-900 flex bg-blue-200 justify-center items-center p-24"> {data[currentQuestion].question} </div><br/>
-            <div className="flex justify-between items-center md:space-x-32">
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={1} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer1}</button><br/>
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={2} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer2}</button>
-            </div>
-            <div className="flex justify-between items-center md:space-x-32">
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={3} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer3}</button><br/>
-                <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" value={4} onClick={(e) => handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} >{data[currentQuestion].answer4}</button>
-            </div>
+                <div className="text-blue-900 flex bg-blue-200 justify-center items-center p-24"> 
+                    {data[currentQuestion].question} 
+                </div>
+                <br/>
+                <div className="flex justify-between items-center md:space-x-32">
+                    <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" 
+                        value={1} 
+                        onClick={(e) => 
+                            handleAnswerOptionClick(
+                                data[currentQuestion].answer.toString(), e.target.value
+                            )}
+                    >
+                        {data[currentQuestion].answer1}
+                    </button>
+                    <br/>
+                    <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" 
+                        value={2} 
+                        onClick={(e) => 
+                            handleAnswerOptionClick(
+                                data[currentQuestion].answer.toString(), e.target.value
+                            )}
+                    >
+                        {data[currentQuestion].answer2}
+                    </button>
+                </div>
+                <div className="flex justify-between items-center md:space-x-32">
+                    <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" 
+                        value={3} 
+                        onClick={(e) => 
+                            handleAnswerOptionClick(
+                                data[currentQuestion].answer.toString(), e.target.value
+                            )}>
+                        {data[currentQuestion].answer3}
+                    </button>
+                    <br/>
+                    <button className="mr-2 transition duration-500 ease-in-out hover:bg-blue-400 transform hover:-translate-y-1 hover:scale-100 rounded-md bg-blue-900 hover:bg-blue-500  border rounded-md p-2 my-2 w-1/2 text-white font-bold font-sans" 
+                        value={4} 
+                        onClick={(e) => 
+                        handleAnswerOptionClick(data[currentQuestion].answer.toString(), e.target.value)} 
+                    >
+                        {data[currentQuestion].answer4}
+                    </button>
+                </div>
             </div>
            </> )}
         </>
--- src/component/attendees/AttendeesLobby.js
+++ src/component/attendees/AttendeesLobby.js
@@ -7,16 +7,26 @@ function AttendeesLobby(props) {
         <div className="bg-blue-900 min-h-screen md:px-20 md:py-10 p-5">
             <div className="flex justify-between">
                 <div>
-                   <p className="ml-2 text-white text-3xl font-bold">{props.location.state.subject}</p>
-                    <p className="mt-3 bg-blue-200 rounded-2xl px-1 text-xl px-4 text-blue-900">{props.location.state.code}</p>
+                   <p className="ml-2 text-white text-3xl font-bold">
+                       {props.location.state.subject}
+                    </p>
+                    <p className="mt-3 bg-blue-200 rounded-2xl px-1 text-xl px-4 text-blue-900">
+                        {props.location.state.code}
+                    </p>
                 </div>
                 <div className='p-3'>
-                    <button className='transition duration-200 ease-in-out hover:bg-white transform text-blue-900 hover:scale-500 text-4xl bg-blue-200 bg-green-200 focus:bg-green-300 rounded-md w-auto p-4 px-16'>Ready</button>
+                    <button className='transition duration-200 ease-in-out hover:bg-white transform text-blue-900 hover:scale-500 text-4xl bg-blue-200 bg-green-200 focus:bg-green-300 rounded-md w-auto p-4 px-16'>
+                        Ready
+                    </button>
                 </div>
             </div>
             <div className='pt-5 space-y-10'>
                 <div className='flex justify-center'>
-                    <input className='rounded-md text-center md:w-1/3 py-2' placeholder='Enter your name' value={student} onChange={(e)=>setStudent(e.target.value)}/>
+                    <input className='rounded-md text-center md:w-1/3 py-2' 
+                        placeholder='Enter your name' 
+                        value={student} 
+                        onChange={(e)=>setStudent(e.target.value)}
+                    />
                 </div>
 
                 <div className='grid md:grid-cols-2 md:grid-rows-5 gap-y-1 md:px-64 justify-items-center '>
